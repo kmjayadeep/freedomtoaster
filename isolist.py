5ba20932fab94458c479d2733c9d618c47e5df9a
Jayadeep KM <kmjayadeep@gmail.com>
2016-03-01 21:53:12 +0530

    isolist added

---
+++ isolist.py
@@ -0,0 +1,50 @@
+import os
+import re
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+numIsos = 0
+class Iso:
+    def __init__(self):
+        self.name = ''
+        self.description = ''
+        self.longdescription = ''
+        self.image = ''
+        self.filename = ''
+
+def getIsoList():    
+    numIsos = 0          
+    isoList = []
+    filelist = os.listdir('iso/')
+    filelist.sort()
+
+    # for every file in the directory
+    for filename in filelist:
+        if re.search('\.xml$', filename):
+            iso = Iso()
+            DOMTree = parse('iso/'+filename)
+
+            node = DOMTree.documentElement
+
+            name = node.getElementsByTagName('name')
+            iso.name = name[0].childNodes[0].data
+                    
+            name = node.getElementsByTagName('description')
+            iso.description = name[0].childNodes[0].data
+                    
+            name = node.getElementsByTagName('longdescription')
+            iso.longdescription = name[0].childNodes[0].data
+                    
+            name = node.getElementsByTagName('image')
+            iso.image = name[0].childNodes[0].data
+
+            name = node.getElementsByTagName('filename')
+            iso.filename = name[0].childNodes[0].data
+
+            isoList.append(iso)
+            numIsos += 1
+
+    return isoList
+
+def getNoIsos():
+           return numIsos
