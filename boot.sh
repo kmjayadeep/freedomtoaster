332458e25fda0f8b17cb5ab2bca4d2e9766de54c
Nisham <moonblade168@gmail.com>
2016-03-02 20:15:00 +0530

    test done

---
+++ boot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo mkdir -p /media/iso/
+sudo mount -o loop ubuntu-gnome-15.04-desktop-i386.iso /media/iso
+sudo mkdir -p /media/toBurn
+sudo mount /dev/sdb /media/toBurn
+cp -a /media/iso/. /media/toBurn
+# sudo apt-get install syslinux mtools
+sudo syslinux -s /dev/sdb
+mv /media/toBurn/isolinux /media/toBurn/syslinux
+mv /media/toBurn/syslinux/isolinux.cfg /media/toBurn/syslinux/syslinux.cfg
+
--- test.py
+++ test.py
@@ -2,23 +2,53 @@
 import gi
 import json
 import isolist
+import inspect
+print(inspect.getfile(gi))
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
 class Window(gtk.Window):
-	def __init__(self,myTitle):
+	def __init__(self,myTitle,isoList):
+		self.isoList = isoList
 		gtk.Window.__init__(self,title=myTitle)
-		button=gtk.Button("click")
-		button.connect("clicked",self.onButtonClick)
-		self.add(button)
+		self.set_border_width(10)
+		self.set_default_size(300, 250)
 
-	def onButtonClick(self,widget):
-		print("clicked")
+		header = gtk.HeaderBar(title = myTitle)
+		header.set_subtitle("subtitle")
+		header.props.show_close_button=True
 
-isoList = isolist.getIsoList()
-for iso in isoList:
-	iso.printDetails()
+		
+		scrolled = gtk.ScrolledWindow()
+		scrolled.set_policy(gtk.PolicyType.NEVER, gtk.PolicyType.AUTOMATIC)
+		
+		flowbox = gtk.FlowBox()
+		flowbox.set_valign(gtk.Align.START)
+		flowbox.set_max_children_per_line(30)
+		flowbox.set_selection_mode(gtk.SelectionMode.NONE)
 
-win = Window("test")
-win.connect("delete-event",gtk.main_quit)
-win.show_all()
-gtk.main()
+		self.addStuff(flowbox)
+		scrolled.add(flowbox)
+
+
+		self.add(scrolled)
+		self.show_all()
+
+	def addStuff(self,flowbox):		
+		for iso in self.isoList:
+			button=gtk.Button(iso.name)
+			button.connect("clicked",self.onButtonClick,iso.name)
+			flowbox.add(button)
+
+
+	def onButtonClick(self,widget,name):
+		iso = next(x for x in self.isoList if x.name==name)
+		
+
+def main():
+		isoList = isolist.getIsoList()
+		win = Window("test",isoList)
+		win.connect("delete-event",gtk.main_quit)
+		win.show_all()
+		gtk.main()	
+
+main()
